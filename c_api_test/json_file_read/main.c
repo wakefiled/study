3526950da1f11d7b81c10bb57c616ea6c034a928
wakefiled <oiu124@naver.com>
2018-08-21 19:53:14 +0900

    [wakefiled] Modified the json parser.

--- c_api_test/json_file_read/main.c
+++ c_api_test/json_file_read/main.c
@@ -5,18 +5,100 @@
 
 #define FILE_NAME_LEN (64)
 
+#define PRINT_KEY_VALUE(type,obj) \
+	switch(type){ \
+		case json_type_null:\
+			printf("Type is null\n");\
+			break;\
+		case json_type_boolean:\
+			printf("%d\n",json_object_get_boolean(obj));\
+			break;\
+		case json_type_double:\
+			printf("%f\n",json_object_get_double(obj));\
+			break;\
+		case json_type_int:\
+			printf("%d\n",json_object_get_int(obj));\
+			break;\
+		case json_type_object:\
+			printf("%ld\n",json_object_get_int64(obj));\
+			break;\
+		case json_type_array:\
+			printf("%s\n",json_object_get_string(obj));\
+			break;\
+		case json_type_string:\
+			printf("%s\n",json_object_get_string(obj));\
+			break;\
+	}
+
+void parseJsonObject1(json_object * pInput)
+{
+	json_object * pJsonObject = NULL;
+	int type = 0;
+
+	pJsonObject = json_object_object_get(pInput,"name");
+	type = json_object_get_type(pJsonObject);
+	printf("[ex1]");
+	PRINT_KEY_VALUE(type,pJsonObject);
+}
+
+void parseJsonObject2(json_object * pInput)
+{
+	json_object * pJsonObject = NULL;
+	int type = 0;
+
+	json_object_object_foreach(pInput, key_name, value_obj)
+	{
+		printf("[ex2] %s\n",key_name);
+		type = json_object_get_type(value_obj);
+		PRINT_KEY_VALUE(type,value_obj);
+	}
+}
+
+void parseJsonObject3(json_object * pInput)
+{
+	json_object * temp = NULL;
+	char * name = NULL;
+	struct json_object_iterator iter;
+	struct json_object_iterator itEnd;
+	int type = 0;
+
+	iter  = json_object_iter_begin(pInput);
+	itEnd = json_object_iter_end(pInput);
+
+	while(!json_object_iter_equal(&iter, &itEnd))
+	{
+		name = json_object_iter_peek_name(&iter);
+		temp = json_object_iter_peek_value(&iter);
+		type = json_object_get_type(temp);
+		printf("[ex3] %s\n",name);
+		PRINT_KEY_VALUE(type,temp);
+		json_object_iter_next(&iter);
+	}
+}
+
+void parseJsonArray1(json_object * pInput)
+{
+	json_object * pJsonObject = NULL;
+	int type = 0;
+	int i = 0;
+
+	pJsonObject = json_object_object_get(pInput,"hobby");
+	if( json_object_get_type(pJsonObject) != json_type_array) return;
+
+	for( i = 0 ; i < json_object_array_length(pJsonObject) ; i++)
+	{
+		json_object * temp = json_object_array_get_idx(pJsonObject,i);
+		type = json_object_get_type(temp);
+		printf("[ex4]");
+		PRINT_KEY_VALUE(type,temp);
+	}
+}
+
 int main(int argc, char * argv[])
 {
 	json_object * pJsonObject = NULL;
-	json_object * pJsonWidgetObject = NULL;
-	json_object * pJsonDebugObject = NULL;
-	json_object * pJsonWindowObject = NULL;
-	json_object * pJsonTitleObject = NULL;
 	char szFileName[FILE_NAME_LEN] = "./default.json";
 	int ret = 0;
-
-	struct json_object_iterator iter;
-	struct json_object_iterator itEnd;
 
 	if( argc == 2 )
 	{
@@ -30,49 +112,15 @@ int main(int argc, char * argv[])
 		printf("Reading file object is failed.");
 		return -1;
 	}
-	printf("[0]%d\n",json_object_get_type(pJsonObject));
-	printf("[0]%s\n",json_object_get_string(pJsonObject));
 
-	pJsonWidgetObject = json_object_object_get(pJsonObject, "widget");
-	printf("[1]%d\n",json_object_get_type(pJsonWidgetObject));
-	printf("[1]%s\n",json_object_get_string(pJsonWidgetObject));
+	parseJsonObject1(pJsonObject);
+	parseJsonObject2(pJsonObject);
+	parseJsonObject3(pJsonObject);
 
-		
-	pJsonDebugObject = json_object_object_get(pJsonWidgetObject, "debug");
-	printf("[2]%d\n",json_object_get_type(pJsonDebugObject));
-	printf("[2]%s\n",json_object_get_string(pJsonDebugObject));
-
-	pJsonWindowObject = json_object_object_get(pJsonWidgetObject, "window");
-	printf("[2]%d\n",json_object_get_type(pJsonWindowObject));
-	printf("[2]%s\n",json_object_get_string(pJsonWindowObject));
-
-	pJsonTitleObject = json_object_object_get(pJsonWindowObject, "title");
-	printf("[3]%d\n",json_object_get_type(pJsonTitleObject));
-	printf("[3]%s\n",json_object_get_string(pJsonTitleObject));
-
-	json_object_object_foreach(pJsonWindowObject, test1, test2)
-	{
-		printf("[5] %s\n",test1);
-		printf("[5] %s\n",json_object_get_string(test2));
-	}
-
-	iter  = json_object_iter_begin(pJsonWindowObject);
-	itEnd = json_object_iter_end(pJsonWindowObject);
-	while(!json_object_iter_equal(&iter, &itEnd))
-	{
-		json_object * temp = json_object_iter_peek_value(&iter);
-		printf("[6] %s\n", json_object_iter_peek_name(&iter));
-		printf("[6] %s\n", json_object_get_string(temp));
-		printf("[6] %d\n", json_object_get_int(temp));
-		printf("[6] %d\n", json_object_get_boolean(temp));
-		printf("[6] %f\n", json_object_get_double(temp));
-		json_object_iter_next(&iter);
-	}
+	parseJsonArray1(pJsonObject);
 
 	json_object_put(pJsonObject);
 	pJsonObject = NULL;
 
 	return 0;
 }
-
-
