eb20b0409b72911286789cd245e9f8b07b43486e
oiu124 <oiu124@naver.com>
2018-11-29 16:16:35 +0900

    [wakefiled] sock merchant for nodejs

---
+++ algorithm/hackerrank/javascript/sock_merchant/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let result = 0;
+    let temp = new Object;
+
+    for (let i of ar)
+    {
+        if (temp[i] == undefined)
+        {
+            temp[i] = 1;
+        }
+        else
+        {
+            temp[i] = undefined;
+            result += 1;    
+        }
+    }
+
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
+
