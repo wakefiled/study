3dc3576b71d37db1430c05a02d14ba48c7b0186a
jskim8 <jskim8@humaxdigital.com>
2019-08-26 18:43:56 +0900

    [wakefiled] Modified CROS options for cross domain.

--- practice/temp/jskim8_httpserver.py
+++ practice/temp/jskim8_httpserver.py
@@ -5,10 +5,29 @@ from single_test import Neurogenie
 import base64
 import shutil
 import json
+import cv2
 from collections import OrderedDict
 
 HOST_NAME = '192.168.10.36' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8080 # Maybe set this to 9000.
+
+def getJsonTest():
+    t1 = datetime.datetime.now()
+    t2 = datetime.datetime.now()
+
+    json_data = {"classifier":None,"Time":str(t2-t1)}
+    out = 0
+
+    if out == 0:
+        json_data["classifier"] = "CAN"
+    elif out == 1:
+        json_data["classifier"] = "PET"
+    else:
+        json_data["classifier"] = "ETC"
+
+    print(json_data)
+
+    return json.dumps(json_data)
 
 def getResult(s, model):
     
@@ -34,6 +53,35 @@ def getResult(s, model):
     print('[Classify Time]: ' + str(t2-t1))
     print("Model :{}".format(out))
 
+    json_data = {"classifier":None,"time":str(t2-t1)}
+
+    if out == 0:
+        json_data["classifier"] = "CAN"
+    elif out == 1:
+        json_data["classifier"] = "PET"
+    else:
+        json_data["classifier"] = "ETC"
+
+    print(json_data)
+
+    return json.dumps(json_data)
+
+
+def getSelfResult(s, camera, model):
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    ret, frame = camera.read()
+
+    img_name = "./jskim8_get.png"
+    cv2.imwrite(img_name, frame)
+
+    t1 = datetime.datetime.now()
+    out = model.test_net_single(img_name)
+    t2 = datetime.datetime.now()
+
+    print('[Classify Time]: ' + str(t2-t1))
+    print("Model :{}".format(out))
+
     json_data = OrderedDict()
 
     if out == 0:
@@ -46,6 +94,7 @@ def getResult(s, model):
     print(json_data)
 
     return json_data
+
 
 def getImage(s):
     model = Neurogenie()
@@ -84,6 +133,7 @@ def getImage(s):
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     superbin = Neurogenie()
+    camera = cv2.VideoCapture(0)
 
     def __init__(self,request, client_address,server):
         BaseHTTPServer.BaseHTTPRequestHandler.__init__(self,request, client_address,server)
@@ -98,16 +148,28 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.send_header('Access-Control-Allow-Origin', '*')
         s.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         s.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+        s.end_headers()
 
     def do_GET(s):
         """Respond to a GET request."""
         s.send_response(200)
+        s.send_header('Access-Control-Allow-Credentials', 'true')
+        s.send_header('Access-Control-Allow-Origin', '*')
+        s.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        s.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
         s.send_header("Content-type", "text/html")
         s.end_headers()
+
         if s.path == "/hello":
             s.wfile.write("<html><head><title>Hello, world</title></head>");
             s.wfile.write("<body><p>Hello, world.</p>")
             s.wfile.write("</body></html>")
+        elif s.path == "/jetson/cls/self":
+            s.send_response(200)
+            s.send_header('Content-type', 'application/json')
+            s.end_headers()
+            result = getSelfResult(s,MyHandler.camera,MyHandler.superbin)
+            s.wfile.write(result)
         else:
             s.wfile.write("<html><head><title>Title goes here.</title></head>")
             s.wfile.write("<body><p>This is a test.</p>")
@@ -115,8 +177,13 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             s.wfile.write("</body></html>")
     
     def do_POST(s):
+        s.send_response(200)
+        s.send_header('Access-Control-Allow-Credentials', 'true')
+        s.send_header('Access-Control-Allow-Origin', '*')
+        s.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        s.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+        
         if s.path == "/jetson/cls/result":
-            s.send_response(200)
             s.send_header('Content-type', 'application/json')
             s.end_headers()
             result = getResult(s,MyHandler.superbin)
@@ -127,13 +194,12 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             s.end_headers()
             result = getImage(s)
             s.wfile.write(result)
-        elif s.path == "/hello":
+        elif s.path == "/jsontest":
             s.send_response(200)
-            s.send_header('Content-type', 'text/html')
+            s.send_header('Content-type', 'application/json')
             s.end_headers()
-            s.wfile.write("<html><head><title>Hello, world</title></head>");
-            s.wfile.write("<body><p>Hello, world.</p>")
-            s.wfile.write("</body></html>")
+            result = getJsonTest()
+            s.wfile.write(result)
         else:
             s.send_response(200)
             s.send_header('Content-type', 'application/json')
